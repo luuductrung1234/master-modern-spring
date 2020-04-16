4121881ea5d4cb4ef899be9dbf7798688ec865e8
Luu Duc Trung <luuductrung1234@gmail.com>
2020-04-16 21:29:18 +0700

    read setting from properties file

---
+++ src/main/java/com/learning/mastermodernspring/properties/ProductExternalService.java
@@ -0,0 +1,23 @@
+package com.learning.mastermodernspring.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductExternalService {
+
+    // setting from property file
+    @Value("${external.productservice.url}")
+    private String url;
+
+    public ProductExternalService() {
+        super();
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+}
