9ae899581f47138c5c20bf2c5abbaca3ed7eaf5a
Luu Duc Trung <luuductrung1234@gmail.com>
2020-04-19 17:16:03 +0700

    disable 2 spring boot application

--- database-demo/src/main/java/com/learning/databasedemo/SpringJpaDemoApplication.java
+++ database-demo/src/main/java/com/learning/databasedemo/SpringJpaDemoApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringJpaDemoApplication implements CommandLineRunner {
 
     private Logger logger = LoggerFactory.getLogger(SpringJpaDemoApplication.class);
