eb86a7889268fcefe5146981e51f815e8f208b17
Luu Duc Trung <luuductrung1234@gmail.com>
2020-04-26 10:47:37 +0700

    move javaee servlet to "jee" sub package

--- first-web-application/src/main/java/webapp/UserValidationService.java
+++
@@ -1,7 +0,0 @@
-package webapp;
-
-public class UserValidationService {
-    public boolean isValidUser(String username, String password) {
-        return username.equals("luuductrung1234") && password.equals("P@ssword1");
-    }
-}
